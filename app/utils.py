5e052fd3a7593cd6cb7e6dbdaaa65c4d499ee289
Gespenst0 <101336424+Gespenst0@users.noreply.github.com>
2022-05-20 15:11:59 +0500

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,9 @@
 from flask import jsonify
-from app.models import Movie, Movie_Schema
+from app.models import Movie, MovieSchema
 
 
 def get_movies_by_director(director_id=None):
-    movies_schema = Movie_Schema(many=True)
+    movies_schema = MovieSchema(many=True)
     if director_id:
         movies = Movie.query.filter(Movie.director_id == director_id)
     else:
@@ -12,18 +12,18 @@ def get_movies_by_director(director_id=None):
 
 
 def get_movies_by_genre(genre_id):
-    movies_schema = Movie_Schema(many=True)
+    movies_schema = MovieSchema(many=True)
     movies = Movie.query.filter(Movie.genre_id == genre_id)
     return movies_schema.dump(movies)
 
 
 def get_movies_by_year(year):
-    movies_schema = Movie_Schema(many=True)
+    movies_schema = MovieSchema(many=True)
     movies = Movie.query.filter(Movie.year == year)
     return movies_schema.dump(movies)
 
 
 def get_movies_by_both_parameters(director_id, genre_id):
-    movies_schema = Movie_Schema(many=True)
+    movies_schema = MovieSchema(many=True)
     movies = Movie.query.filter(Movie.genre_id == genre_id).filter(Movie.director_id == director_id)
     return jsonify(movies_schema.dump(movies))
