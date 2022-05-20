9f02ab46686c20cc3cd48cdf36cab589e0f8aa97
Gespenst0 <101336424+Gespenst0@users.noreply.github.com>
2022-05-20 15:08:35 +0500

    Update genres.py

--- views/genres.py
+++ views/genres.py
@@ -1,14 +1,15 @@
 from flask import jsonify
 from flask_restx import Namespace, Resource
 from app.utils import get_movies_by_genre
-from app.models import Genre,Genre_Schema
+from app.models import Genre, GenreSchema
 
 genres_ns = Namespace('genres')
+
 
 @genres_ns.route('/')
 class GenresView(Resource):
     def get(self):
-        genre_schema = Genre_Schema(many=True)
+        genre_schema = GenreSchema(many=True)
         genres = Genre.query.all()
         if not genres:
             return "", 404
@@ -18,7 +19,7 @@ class GenresView(Resource):
 @genres_ns.route('/<int:gid>')
 class GenreView(Resource):
     def get(self, gid):
-        genre_schema = Genre_Schema()
+        genre_schema = GenreSchema()
         genre = Genre.query.get(gid)
         if not genre:
             return "", 404
